28c99a34504076238222dcf9a78e7a89494153c9
abhi <abhisadineni@gmail.com>
2025-03-28 10:24:08 +0530

    added docker file and requirements.txt

--- backend/db.py
+++ backend/db.py
@@ -1,14 +1,6 @@
 from pymongo.mongo_client import MongoClient
-from urllib.parse import quote_plus
 
-username = "sadineniabhi"
-password = quote_plus("Venkanna@9")  # Escapes the '@' symbol
-
-# Construct the URI using the escaped password
-uri = f"mongodb://{username}:{password}@ac-qmcaxdu-shard-00-00.nmubbyu.mongodb.net:27017," \
-      f"ac-qmcaxdu-shard-00-01.nmubbyu.mongodb.net:27017," \
-      f"ac-qmcaxdu-shard-00-02.nmubbyu.mongodb.net:27017/" \
-      f"?ssl=true&replicaSet=atlas-av4w2f-shard-0&authSource=admin&retryWrites=true&w=majority&appName=Cluster0"
+uri = "mongodb://abhi2:abhi2@ac-kds5qtp-shard-00-00.ein2vfa.mongodb.net:27017,ac-kds5qtp-shard-00-01.ein2vfa.mongodb.net:27017,ac-kds5qtp-shard-00-02.ein2vfa.mongodb.net:27017/?ssl=true&replicaSet=atlas-7cuyex-shard-0&authSource=admin&retryWrites=true&w=majority&appName=Cluster0"
 
 # Create a new client and connect to the server
 client = MongoClient(uri)
